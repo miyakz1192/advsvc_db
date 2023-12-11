79f2ffd4fbc8d0d777dcb19e45d74d6c98a805b5
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2023-12-11 15:20:44 +0000

    DialogRecord Status added

--- dbmodels.py
+++ dbmodels.py
@@ -2,12 +2,22 @@ from sqlalchemy.types import Integer, String, LargeBinary, DateTime, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 from sqlalchemy import Column
+import uuid
 
 
 Base = declarative_base()
 
 
 class DialogRecord(Base):
+    class Status:
+        INIT = "INIT"
+        AUDIO2TEXT_START = "AUDIO2TEXT_START"
+        AUDIO2TEXT_NOW = "AUDIO2TEXT_NOW"
+        AUDIO2TEXT_END = "AUDIO2TEXT_END"
+        TEXT2ADVICE_START = "TEXT2ADVICE_START"
+        TEXT2ADVICE_NOW = "TEXT2ADVICE_NOW"
+        TEXT2ADVICE_END = "TEXT2ADVICE_END"
+
     __tablename__ = 'dialogs'
 
     id = Column(Integer, primary_key=True)
@@ -23,3 +33,7 @@ class DialogRecord(Base):
     # for optimistic lock
     __mapper_args__ = {'version_id_col': version}
 #    version = Column(Integer, default=0, server_default='0', onupdate=1)
+
+    def __init__(self):
+        self.uuid = str(uuid.uuid4())
+        self.status = DialogRecord.Status.INIT
