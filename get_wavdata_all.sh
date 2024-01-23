55acae5ff005d3a6b5874c4de55086a679ffa50e
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2024-01-23 15:00:10 +0000

    utility modified

--- get_wavdata_all.sh
+++ get_wavdata_all.sh
@@ -1,1 +1,4 @@
-./dbopetool.py  --listrec  | grep RECORD | awk -F "=" '{print $2}' | while read line; do ./dbopetool.py --savewav ${line}; done
+
+./dbopetool.py --listrec | awk -F "ID=" '{print $2}' | awk -F "," '{print $1}' | grep -v "^$" | while read line; do ./dbopetool.py --savewav ${line}; done
+
+# ./dbopetool.py  --listrec  | grep "ID=" | awk -F "ID=" '{print $2}' | while read line; do ./dbopetool.py --savewav ${line}; done
